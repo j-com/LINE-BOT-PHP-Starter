a17286525d8a850bf9d3fe1688947aea160f8d58
j-com <kh.suphakrit@gmail.com>
2017-03-24 11:54:37 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -22,6 +22,19 @@ if (!is_null($events['events'])) {
 				'text' => $text
 			];
 		}
+		
+		if($event['type'] == 'message' && $event['message']['type'] == 'sticker')){
+			// Get text sent
+			$text = $event['message']['sticker'];
+			// Get replyToken
+			$replyToken = $event['replyToken'];
+
+			// Build message to reply back
+			$messages = [
+				'type' => 'sticker',
+				'text' => $text
+			];
+		}
 			// Make a POST Request to Messaging API to reply to sender
 			$url = 'https://api.line.me/v2/bot/message/reply';
 			$data = [
